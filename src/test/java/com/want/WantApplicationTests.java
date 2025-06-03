d78e4ba0fe964453be42c62ffd90e0650d8c1938
LimdaeIl <optional94@naver.com>
2025-06-03 12:09:39 +0900

    🎉#1: 프로젝트 초기화
    프로젝트를 생성
    - gradle - groovy
    - java 17
    - spring boot 3.4.6
    
    dependencies
    - spring web
    - lombok
    
    https://start.spring.io/

---
+++ src/test/java/com/want/WantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.want;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
