ff38869295b3493a9c027a78f95e0cc9245e7049
LimdaeIl <optional94@naver.com>
2025-06-04 15:32:38 +0900

    ✨#17: JpaAuditingConfig으로 Auditing 활성화
    Auditing 기능 활성화

---
+++ src/main/java/com/want/common/audit/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.want.common.audit;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
+public class JpaAuditingConfig {
+}
