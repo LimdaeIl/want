dbaa620725db4c0b3114178c14457bbb1e32f31e
LimdaeIl <optional94@naver.com>
2025-06-04 14:06:11 +0900

    ✨#14: dotenv로 .env를 시스템 프로퍼티에 등록
    application.yml에서 읽어야 할 .env를 찾도록 dotenv를 통해 시스템 프로퍼티에 등록한다.

--- src/main/java/com/want/WantApplication.java
+++ src/main/java/com/want/WantApplication.java
@@ -1,5 +1,6 @@
 package com.want;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WantApplication {
 
 	public static void main(String[] args) {
+
+		// .env 파일 로드 (resources 기준 경로로)
+		Dotenv dotenv = Dotenv.configure()
+				.directory("src/main/resources") // 또는 .env 의 실제 경로
+				.ignoreIfMissing()
+				.load();
+
+		// 시스템 프로퍼티에 등록 (Spring 이 인식할 수 있도록)
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+
 		SpringApplication.run(WantApplication.class, args);
 	}
 
