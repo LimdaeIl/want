83237f54e5c05c23d843a02e93461e2015ffdcae
LimdaeIl <optional94@naver.com>
2025-06-05 14:15:07 +0900

    ✨#21: JWT Provider 설정

---
+++ src/main/java/com/want/common/infrastructure/jwt/JwtProvider.java
@@ -0,0 +1,24 @@
+package com.want.common.infrastructure.jwt;
+
+
+import com.want.user.domain.user.Role;
+
+public interface JwtProvider {
+  String createAccessToken(Long userId, Role role);
+
+  String refreshAccessToken(Long userId);
+
+  long getRefreshTokenExpire();
+
+  long getRemainingMillisByToken(String bearerToken);
+
+  String getTokenId(String bearerToken);
+
+  Long getUserIdByToken(String bearerToken);
+
+  Long getUserId(String bearerToken);
+
+  String getEmail(String bearerToken);
+
+  String getRole(String bearerToken);
+}
