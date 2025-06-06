fa3d7bbfae81a7a521952da9c7f44ea0ca691308
LimdaeIl <optional94@naver.com>
2025-06-06 17:19:05 +0900

    ♻️#24: 로그인 dto 수정

--- src/main/java/com/want/user/application/dto/auth/request/SignInRequest.java
+++ src/main/java/com/want/user/application/dto/auth/request/SignInRequest.java
@@ -11,7 +11,7 @@ public record SignInRequest(
     String email,
     @NotBlank(message = "비밀번호: 비밀번호는 필수입니다.")
     @Pattern(
-        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%?&]{8,}$",
+        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%?&])[A-Za-z\\d@$!%?&]{8,}$",
         message = "비밀번호: 비밀번호는 최소 8자 이상이며, 영문자, 숫자, 특수문자(@, $, !, %, ?, &)를 포함해야 합니다.")
     String password
 ) {
--- src/main/java/com/want/user/application/dto/auth/response/SignInResponse.java
+++ src/main/java/com/want/user/application/dto/auth/response/SignInResponse.java
@@ -3,10 +3,10 @@ package com.want.user.application.dto.auth.response;
 import com.want.user.domain.user.User;
 
 public record SignInResponse(
-    String id,
+    Long id,
     String accessToken
 ) {
   public static SignInResponse from(User user, String accessToken) {
-    return new SignInResponse(user.getEmail(), accessToken);
+    return new SignInResponse(user.getId(), accessToken);
   }
 }
