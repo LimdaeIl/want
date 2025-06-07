d97a49b8771a38a2bffe736fd1177667531a4bea
LimdaeIl <optional94@naver.com>
2025-06-07 12:51:36 +0900

    ✨#24: spring security logout api추가

--- src/main/java/com/want/common/config/SecurityConfig.java
+++ src/main/java/com/want/common/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
   private static final String[] AUTH_WHITELIST = {
       "/api/v1/auth/signup",
       "/api/v1/auth/sign-in",
+      "/api/v1/auth/sign-out"
   };
 
   @Bean
