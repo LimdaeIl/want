fca035d192172ff0168b1ce5d0644b459c38e55c
LimdaeIl <optional94@naver.com>
2025-06-09 10:00:06 +0900

    ♻️37: 이메일 인증 요청 dto 패키지 이동

--- src/main/java/com/want/user/application/dto/auth/request/VerifyEmailCodeRequest.java
+++ src/main/java/com/want/user/application/dto/auth/request/VerifyEmailCodeRequest.java
@@ -1,11 +1,10 @@
-package com.want.user.application.dto.auth.response;
+package com.want.user.application.dto.auth.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 public record VerifyEmailCodeRequest(
 
